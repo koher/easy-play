df10a2c545c9123c1ec4f54a25f0af3e0a35e1ac
Yuta Koshizawa <koher@koherent.org>
2020-04-28 03:04:43 +0900

    Implement `Player` for `.video` source

--- Sources/EasyPlay/PlayerImplementationWithCamera.swift
+++ Sources/EasyPlay/PlayerImplementationWithCamera.swift
@@ -1,6 +1,83 @@
 import Foundation
 import AVFoundation
 
+extension Player.VideoSource {
+    public static func camera(
+        device: AVCaptureDevice,
+        sessionPreset: AVCaptureSession.Preset = .vga640x480,
+        videoSettings: [String: Any] = [:]
+    ) -> Self {
+        Self {
+            try PlayerImplementationWithCamera(
+                device: device,
+                sessionPreset: sessionPreset,
+                videoSettings: videoSettings
+            )
+        }
+    }
+    
+    public static func camera(
+        position: AVCaptureDevice.Position = .back,
+        focusMode: AVCaptureDevice.FocusMode? = nil,
+        sessionPreset: AVCaptureSession.Preset = .vga640x480,
+        videoSettings: [String: Any] = [:]
+    ) -> Self {
+        Self {
+            var deviceOrNil: AVCaptureDevice? = nil
+            if #available(OSX 10.15, iOS 10.0, *) {
+                deviceOrNil = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: position)
+            } else {
+                for aDevice in AVCaptureDevice.devices(for: .video) {
+                    if aDevice.position == position {
+                        deviceOrNil = aDevice
+                        break
+                    }
+                }
+            }
+            guard let device = deviceOrNil else {
+                throw Player.CameraInitializationError.unsupportedPosition(position)
+            }
+
+            do {
+                try device.lockForConfiguration()
+                defer { device.unlockForConfiguration() }
+                
+                if let focusMode = focusMode {
+                    guard device.isFocusModeSupported(focusMode) else {
+                        throw Player.CameraInitializationError.unsupportedFocusMode(focusMode)
+                    }
+                    device.focusMode = focusMode
+                } else {
+                    if device.isFocusModeSupported(.continuousAutoFocus) {
+                        device.focusMode = .continuousAutoFocus
+                    } else if device.isFocusModeSupported(.autoFocus) {
+                        device.focusMode = .autoFocus
+                    }
+                }
+            } catch let error as Player.CameraInitializationError {
+                throw error
+            } catch let error {
+                throw Player.CameraInitializationError.configurationFailure(error)
+            }
+            
+            return try PlayerImplementationWithCamera(
+                device: device,
+                sessionPreset: sessionPreset,
+                videoSettings: videoSettings
+            )
+        }
+    }
+}
+
+extension Player {
+    public enum CameraInitializationError: Error {
+        case unsupportedPosition(AVCaptureDevice.Position)
+        case unsupportedFocusMode(AVCaptureDevice.FocusMode)
+        case unsupportedSessionPreset(AVCaptureSession.Preset)
+        case configurationFailure(Error)
+    }
+}
+
 internal final class PlayerImplementationWithCamera: PlayerImplementation {
     private let session: AVCaptureSession
     private let sampleBufferDelegate: SampleBufferDelegate
@@ -44,7 +121,10 @@ internal final class PlayerImplementationWithCamera: PlayerImplementation {
         handler != nil
     }
     
-    func play(_ handler: @escaping (CVPixelBuffer) -> Void) -> Bool {
+    func play(
+        _ handler: @escaping (CVPixelBuffer) -> Void,
+        completion: ((Error?) -> Void)?
+    ) -> Bool {
         synchronized {
             if _isPlaying { return false }
             
---
+++ Sources/EasyPlay/PlayerImplementationWithVideo.swift
@@ -0,0 +1,179 @@
+import Foundation
+import AVFoundation
+
+extension Player.VideoSource {
+    public static func video(
+        asset: AVAsset,
+        outputSettings: [String: Any]? = [
+            kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange,
+        ]
+    ) -> Self {
+        .init {
+            try PlayerImplementationWithVideo(asset: asset, outputSettings: outputSettings)
+        }
+    }
+    
+    public static func video(
+        url: URL,
+        outputSettings: [String: Any]? = [
+            kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange,
+        ]
+    ) -> Self {
+        .init {
+            let asset: AVAsset = .init(url: url)
+            return try PlayerImplementationWithVideo(asset: asset, outputSettings: outputSettings)
+        }
+    }
+    
+    public static func video(
+        path: String,
+        outputSettings: [String: Any]? = [
+            kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange,
+        ]
+    ) -> Self {
+        .init {
+            let asset: AVAsset = .init(url: URL(fileURLWithPath: path))
+            return try PlayerImplementationWithVideo(asset: asset, outputSettings: outputSettings)
+        }
+    }
+}
+
+extension Player {
+    public enum VideoInitializationError: Error {
+        case noVideoTrack
+        case multipleVideoTracks(Int)
+        case readerInitializationFailure(Error)
+    }
+    
+    public enum VideoPlayerError: Error {
+        case readingFailed(Error)
+        case unknown
+    }
+}
+
+internal final class PlayerImplementationWithVideo: PlayerImplementation {
+    private let videoTrack: AVAssetTrack
+    private let reader: AVAssetReader
+    private let output: AVAssetReaderTrackOutput
+
+    private var timer: DispatchSourceTimer
+    
+    private var handler: ((CVPixelBuffer) -> Void)?
+    private var completion: ((Error?) -> Void)?
+    
+    private let lock: NSRecursiveLock = .init()
+    
+    init(asset: AVAsset, outputSettings: [String: Any]?) throws {
+        let videoTracks = asset.tracks(withMediaType: .video)
+        guard let videoTrack = videoTracks.first else {
+            throw Player.VideoInitializationError.noVideoTrack
+        }
+        guard videoTracks.count == 1 else {
+            throw Player.VideoInitializationError
+                .multipleVideoTracks(videoTracks.count)
+        }
+        self.videoTrack = videoTrack
+        
+        do {
+            reader = try AVAssetReader(asset: asset)
+        } catch let error {
+            throw Player.VideoInitializationError
+                .readerInitializationFailure(error)
+        }
+        output = .init(track: videoTrack, outputSettings: outputSettings)
+        output.alwaysCopiesSampleData = false
+        reader.add(output)
+        
+        timer = DispatchSource.makeTimerSource(flags: .strict, queue: .global())
+        timer.schedule(
+            deadline: .now(),
+            repeating: 1.0 / TimeInterval(videoTrack.nominalFrameRate)
+        )
+        
+        reader.startReading()
+    }
+    
+    deinit {
+        _ = pause()
+        timer.cancel()
+        reader.cancelReading()
+    }
+
+    var isPlaying: Bool {
+        synchronized { _isPlaying }
+    }
+    
+    private var _isPlaying: Bool {
+        handler != nil
+    }
+
+    func play(
+        _ handler: @escaping (CVPixelBuffer) -> Void,
+        completion: ((Error?) -> Void)?
+    ) -> Bool {
+        synchronized {
+            if _isPlaying { return false }
+            
+            self.handler = handler
+            timer.setEventHandler { [weak self] in
+                guard let self = self else { return }
+                guard let sampleBuffer = self.output.copyNextSampleBuffer() else {
+                    switch self.reader.status {
+                    case .completed:
+                        completion?(nil)
+                        assert(self.pause())
+                    case .failed:
+                        if let completion = self.completion {
+                            if let error = self.reader.error {
+                                completion(Player.VideoPlayerError.readingFailed(error))
+                            } else {
+                                completion(Player.VideoPlayerError.unknown)
+                            }
+                        }
+                        assert(self.pause())
+                    case .cancelled:
+                        break
+                    case .reading:
+                        break
+                    case .unknown:
+                        break
+                    @unknown default:
+                        break
+                    }
+                    return
+                }
+                let imageBufferOrNil: CVImageBuffer?
+                if #available(OSX 10.15, iOS 13.0, *) {
+                    imageBufferOrNil = sampleBuffer.imageBuffer
+                } else {
+                    imageBufferOrNil = CMSampleBufferGetImageBuffer(sampleBuffer)
+                }
+                if let imageBuffer = imageBufferOrNil {
+                    assert(self.handler != nil)
+                    self.handler?(imageBuffer)
+                }
+            }
+            timer.resume()
+            
+            return true
+        }
+    }
+    
+    func pause() -> Bool {
+        synchronized {
+            guard _isPlaying else { return false }
+
+            timer.suspend()
+            handler = nil
+            completion = nil
+            
+            return true
+        }
+    }
+
+    fileprivate func synchronized<T>(_ operation: () -> T) -> T {
+        lock.lock()
+        defer { lock.unlock() }
+        return operation()
+    }
+}
